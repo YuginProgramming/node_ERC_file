c73c56a7c0341ca0cad7c6bfcb59b5fb5b007da6
YuginProgramming <dudaryev@gmail.com>
2022-10-13 18:18:30 +0300

    nice13

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ const address = '0xA145ac099E3d2e9781C9c848249E2e6b256b030D'
 const provider = new Web3.providers.HttpProvider(linkApi);
 const web3 = new Web3(provider);
 
-const amount = async () => {
-  const ether = await web3.eth.getBalance(address);
-  const balance = web3.utils.fromWei(ether, 'ether');
-  return balance;
-}
-const logTime = () => {
-  return new Date().format("Y-M-d H:M:S")+' ';
-}
-const createLog = async () => {
-  const balance = await amount();
-  const time = logTime();
-  await fs.appendFile('readme.log', time + balance + '\n');
-};
-createLog();
+server.get('/', (req, res) => {
+
+  const amount = async () => {
+    const ether = await web3.eth.getBalance(address);
+    const balance = web3.utils.fromWei(ether, 'ether');
+    return balance;
+  }
+  const logTime = () => {
+    return new Date().format("Y-M-d H:M:S")+' ';
+  }
+  const createLog = async () => {
+    const balance = await amount();
+    const time = logTime();
+    await fs.appendFile('readme.log', time + balance + '\n');
+    res.end(time + balance);
+  };
+  createLog();
+});
+server.listen(3000);
